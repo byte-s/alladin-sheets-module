5537e6c48aaced8e776788101aae5720c9ad0bce
Ilnaz Niyazov <inigam277@gmail.com>
2025-09-25 21:10:26 +0600

    aals

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,8 +3,21 @@ import { JWT } from 'google-auth-library'
 import creds from '../../quiet-dryad-creds.json'
 import { json } from 'stream/consumers';
 
+export const config = {
+      api: {
+        bodyParser: false, // Disable Next.js's default body parser
+      },
+    };
+
 export async function POST(request: Request) {
-    const res = await request.json();
+    //const res = await request.json();
+    const formData = await request.formData()
+
+    let formDataValues = []
+    for (const value of formData.values()) {
+        formDataValues.push(value.toString());
+    }
+    //const text = formData.get('ttext') || 'ssss'
 
     const SCOPES = [
         'https://www.googleapis.com/auth/spreadsheets',
@@ -22,7 +35,7 @@ export async function POST(request: Request) {
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[0];
 
-    const addRow = await sheet.addRow([res.toString()]);
+    const addRow = await sheet.addRow(formDataValues);
     if(addRow){
         return Response.json({ text:'Успех' })
     } else{
