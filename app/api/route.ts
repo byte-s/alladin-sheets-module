0df2d2f2197b174908739c61f766a56acf2c8172
Ilnaz Niyazov <inigam277@gmail.com>
2025-09-25 15:19:42 +0600

    Добавить API для работы с Google Sheets и обновить зависимости

---
+++ app/api/route.ts
@@ -0,0 +1,32 @@
+import {GoogleSpreadsheet} from 'google-spreadsheet';
+import { JWT } from 'google-auth-library'
+import creds from '../../quiet-dryad-creds.json'
+import { json } from 'stream/consumers';
+
+export async function POST(request: Request) {
+    const res = await request.json();
+
+    const SCOPES = [
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive.file',
+    ];
+
+    const jwt = new JWT({
+        email: creds.client_email,
+        key: creds.private_key,
+        scopes: SCOPES,
+    });
+
+    const doc = new GoogleSpreadsheet('1TRevNsY_rlLxJf4pUqAFTKOuT1esw4ogpY6Os-bknNw', jwt);
+
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+
+    const addRow = await sheet.addRow([JSON.stringify(res.json())]);
+    if(addRow){
+        return Response.json({ text:'Успех' })
+    } else{
+return Response.json({ text:'Не успех' })
+    }
+    
+}
